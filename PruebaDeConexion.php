ee989993bf6bd8d4ccd90cb37b79e85939a4eb87
KevinGarroMonge <kgarro00003@ufide.ac.cr>
2021-07-22 18:09:09 -0600

    Conexion creada

---
+++ PruebaDeConexion.php
@@ -0,0 +1,26 @@
+<?php
+
+// Aquí Kevin debes usar tus credenciales de SQL Developer
+$conn = oci_connect('HR','123','ORCL');
+
+if (!$conn) {
+  // echo '1';
+    $e = oci_error();
+  //  trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$stid = oci_parse($conn, 'SELECT * FROM employees');
+
+oci_execute($stid);
+
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+
+?>
